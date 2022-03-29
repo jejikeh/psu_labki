725903a80244b8063ef1f9984fe5c3503efa2560
jejikeh <jejikx@gmail.com>
2022-03-29 03:53:03 +0300

    oaip

--- oaip/7lab/main.cpp
+++ oaip/7lab/main.cpp
@@ -1,67 +1,33 @@
 #include <iostream>
-
-
-struct node {
-    float data;
-    node *forward;
-    node *back;
-};
-
-struct double_linked_list {
-    node *start;
-    node *end;
-    node *pointer;
-};
-
-void initList(double_linked_list *DLL){
-    // выделение памяти для правого фиктивного элемента
-    DLL->start = (node *) malloc(sizeof(node));
-    // элемент, следующий за левым фиктивным - правый фиктивный элемент
-    DLL->start->forward = DLL->end;
-    // элемент, предшествующий первому - последний элемент
-    DLL->start->back = DLL->end;
-    //  элемент, идущий после последнего элемента - первый элемент
-    DLL->end->forward = DLL->start;
-    // элемент, предшествующий правому фиктивному - левый фиктивный элемент 
-    DLL->end->back = DLL->start;
-
-    DLL->pointer = DLL->start;
-}
-
-void insertPredNode(double_linked_list *DLL, float data){
-    node *newNode = (node *)malloc(sizeof(node));
-    newNode->data = data;
-    newNode->forward = DLL->pointer;
-    newNode->back = DLL->pointer->back;
-    DLL->pointer->back = newNode;
-    DLL->pointer->forward = newNode;
-}
-
-void insertPostNode(double_linked_list *DLL, float data){
-    node *newNode = (node *)malloc(sizeof(node));
-    newNode->data = data;
-    newNode->forward = DLL->pointer->forward;
-    newNode->back = DLL->pointer;
-    DLL->pointer->back = newNode;
-    DLL->pointer->forward = newNode;
-}
-
-void printList(double_linked_list *DLL){
-    node *temp;
-    temp = DLL->start->forward;
-    while(temp != DLL->end){
-        std::cout << temp->data << " ";
-        temp = temp->forward;
+struct node { float data; node *next; node(float c_data,node *c_next){data = c_data; next = c_next;}};
+class linked_list {
+    node *root; // указатель на последний и корневой элемент;
+    public:
+    linked_list(){ root = new node(0,root);} // при инициализации линейного кольцевого списка указываем указатель на корень 
+    node* get_last(node* temp){
+        return temp->next != root ? get_last(temp->next) : temp;
     }
-}
+    void insert_root_node(float data) { // Вставка элемента сначала, меняем корневой элемент на новый
+        node* new_node = new node(data,root); // Создаем новый корень.
+        node *last_node = get_last(root); // Ищем последний элемент.
+        root = new_node; // Меняем корень списка  на новый узел
+        last_node->next = root; // 
+    }
+    void insert_last_node(float data) {
+        node* new_node = new node(data,root); // Создаем новый корень.
+        node *last_node = get_last(root); // Ищем последний элемент.
+        last_node->next = new_node; // теперь последний элемент это новый узел
+    }
+    void print_list(){
+        node* temp = root;
+        do { std::cout << temp->data << " "; temp = temp->next; } while (temp  != root); // До тех пор пока следуюший элемент снова не будет корнем
+    }
+};
 
 int main(){
-    double_linked_list *DDL = (double_linked_list*)malloc(0);
-    initList(DDL);
-    float x = 2;
-    insertPostNode(DDL,x);
-    std::cout << "d";
+    linked_list* list = new linked_list();
+    list->insert_root_node(1);
+    list->insert_last_node(3);
+    list->insert_root_node(2);
+    list->print_list();
 }
-
-
-
