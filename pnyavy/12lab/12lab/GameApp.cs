2f8353e5b45fc112a81f5924d8d60aee3498ea97
jejikeh <jejikx@gmail.com>
2022-04-01 21:36:05 +0300

    12lab and 11lab fix

---
+++ pnyavy/12lab/12lab/GameApp.cs
@@ -0,0 +1,21 @@
+﻿namespace _12lab
+{
+    class GameApp : Application
+    {
+        private string _genre;
+        private int _age;
+        private bool _multiplayer;
+
+        public GameApp(string genre, int age, bool multiplayer,string developer,int version,int year) : base(developer,version,year)
+        {
+            _genre = genre;
+            _age = age;
+            _multiplayer = multiplayer;
+        }
+
+        public void PrintGenre()
+        {
+            Console.WriteLine(_genre);
+        }
+    }
+}
---
+++ pnyavy/12lab/12lab/SienceApp.cs
@@ -0,0 +1,25 @@
+﻿namespace _12lab
+{
+    internal class SienceApp : Application
+    {
+        private string _field_of;
+        private int    _cost;
+        private bool _paralel_calculations;
+
+        public SienceApp(string field_of,int cost,bool paralel_calculations,string developer,int version, int year) : base(developer, version, year)
+        {
+            _field_of = field_of;
+            _cost = cost;
+            _paralel_calculations = paralel_calculations;
+        }
+
+        public void PrintAll()
+        {
+            Console.WriteLine($"Cost {_cost}");
+            Console.WriteLine($"Field {_field_of}");
+            Console.WriteLine($"Paralel calculations of {_paralel_calculations}");
+            PrintBase();
+        }
+
+    }
+}
