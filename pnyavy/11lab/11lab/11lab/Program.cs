fe7018f999ab1a88077facb479cbb2ed4e9edf1c
jejikeh <jejikx@gmail.com>
2022-04-01 20:35:50 +0300

    11lab

--- pnyavy/11lab/11lab/11lab/Program.cs
+++ pnyavy/11lab/11lab/11lab/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace _11lab
+{
+    class Program
+    {
+        static private void Main()
+        {
+            Staff[] staffs = new Staff[4];
+
+            staffs[0] = new Staff("Hello", 3, 5);
+            staffs[0].Print();
+
+            staffs[0].SetName();
+            staffs[0].Print();
+
+            staffs[1] = new Staff("HH");
+            staffs[1].Print();
+
+            staffs[2] = new Staff();
+            staffs[2].Print();
+        }
+    }
+}
---
+++ pnyavy/11lab/11lab/11lab/Staff.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11lab
+{
+    class Staff
+    {
+        private string _name;
+        private int    _number;
+        private int    _rank;
+
+        public Staff(string name)
+        {
+            _name = name;
+        }
+        public Staff(int number, int rank)
+        {
+            _name = "";
+            _number = number;
+            _rank = rank;
+        }
+        public Staff(string name, int number, int rank)
+        {
+            _name = name;
+            _number = number;
+            _rank = rank;
+        }
+        public Staff()
+        {
+            Console.WriteLine("Input name ->");
+            string? name = Console.ReadLine();
+            Console.WriteLine("Input number ->");
+            int number = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Input rank ->");
+            int rank = Convert.ToInt16(Console.ReadLine());
+            if(name != null)
+            {
+                _name = name;
+                _number = number;
+                _rank = rank;
+            }
+
+        }
+        public void SetName()
+        {
+           string? name = Console.ReadLine();
+            if (name != null)
+            {
+                _name = name;
+            } 
+        }
+        public void Print()
+        {
+            Console.WriteLine("Name : " + _name);
+            Console.WriteLine("Number : " + _number);
+            Console.WriteLine("Rank : " + _rank);
+        }
+    }
+}
