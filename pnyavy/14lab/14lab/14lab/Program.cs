f365ebd9650c5c608cbaae8b76de5949997c925a
jejikeh <44530103+jejikeh@users.noreply.github.com>
2022-04-07 14:10:10 +0300

    14lab

---
+++ pnyavy/14lab/14lab/14lab/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _14lab
+{
+
+    class Program
+    {
+        static private void Main()
+        {
+            VectorArray vectorArray = new VectorArray(3);
+
+            vectorArray.Add(1);
+            vectorArray.Add(3);
+            vectorArray.Add(4);
+
+            vectorArray.AddValueToAll(10);
+            vectorArray.Print();
+        }
+    }
+}
---
+++ pnyavy/14lab/14lab/14lab/VectorArray.cs
@@ -0,0 +1,58 @@
+﻿
+namespace _14lab
+{
+    class Node
+    {
+        private int Value;
+        private int Index;
+
+        public Node(int value, int index)
+        {
+            Value = value;
+            Index = index;
+        }
+        internal void Print()
+        {
+            Console.WriteLine("Value -> " + Value + " at " + Index + " index");
+        }
+
+        internal void Add(int value)
+        {
+            Value = Value + value;
+        }
+    }
+
+    
+    class VectorArray
+    {
+        public int CurrentSize = -1;
+        public Node[] Array;
+
+        public VectorArray(int maxSizeArray)
+        {
+            Array = new Node[maxSizeArray];
+        }
+
+        public void Add(int value)
+        {
+            CurrentSize++;
+            Array[CurrentSize] = new Node(value, CurrentSize);
+        }
+
+        public void Print()
+        {
+            for(int i = 0; i <= CurrentSize; i++)
+            {
+                Array[i].Print();
+            }
+        }
+
+        public void AddValueToAll(int value)
+        {
+            for (int i = 0; i <= CurrentSize; i++)
+            {
+                Array[i].Add(value);
+            }
+        }
+    }
+}
