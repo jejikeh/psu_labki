b4c4080144e2ea76824888662e910ac694fd630e
jejikeh <44530103+jejikeh@users.noreply.github.com>
2022-04-12 02:40:13 +0300

    pnyavu 14lab

--- pnyavy/14lab/14lab/14lab/Program.cs
+++ pnyavy/14lab/14lab/14lab/Program.cs
@@ -5,13 +5,22 @@
     {
         static private void Main()
         {
-            VectorArray vectorArray = new VectorArray(3);
+            Console.WriteLine("Input max size of array : ");
+            VectorArray vectorArray = new VectorArray(Convert.ToInt16(Console.ReadLine())); // Создание массива
 
-            vectorArray.Add(1);
-            vectorArray.Add(3);
-            vectorArray.Add(4);
+            for(int i = 0; i < vectorArray.GetMaxValue; i++)
+            {
+                Console.WriteLine("Input value of " + i + " element : ");
+                vectorArray.Add(Convert.ToInt16(Console.ReadLine()));
+            }
+            Console.WriteLine("Root : ");
+            Console.WriteLine(vectorArray.Root()?.GetValue); // Вывод корневого
 
-            vectorArray.AddValueToAll(10);
+            Console.WriteLine("Check equal : ");
+            vectorArray.CheckEqual();
+
+            Console.WriteLine("Add 1 to all : ");
+            vectorArray.AddValueToAll(1);
             vectorArray.Print();
         }
     }
--- pnyavy/14lab/14lab/14lab/VectorArray.cs
+++ pnyavy/14lab/14lab/14lab/VectorArray.cs
@@ -3,43 +3,66 @@ namespace _14lab
 {
     class Node
     {
-        private int Value;
-        private int Index;
+        private int _value;
+        private int _index;
 
+
+        public int GetValue { get { return _value; } }
         public Node(int value, int index)
         {
-            Value = value;
-            Index = index;
+            _value = value;
+            _index = index;
         }
-        internal void Print()
+        internal void Print() // Вывод данных узла
         {
-            Console.WriteLine("Value -> " + Value + " at " + Index + " index");
+            Console.WriteLine("Value -> " + _value + " at " + _index + " index");
         }
 
-        internal void Add(int value)
+        internal void Add(int value) // Добавление значения к узлу
         {
-            Value = Value + value;
+            _value = _value + value;
         }
     }
 
-    
+
     class VectorArray
     {
         public int CurrentSize = -1;
+        private int _maxSize;
         public Node[] Array;
 
-        public VectorArray(int maxSizeArray)
+        public int GetMaxValue { get { return _maxSize; } }
+
+        public VectorArray(int maxSizeArray) // конструктор
         {
             Array = new Node[maxSizeArray];
+            _maxSize = maxSizeArray;
         }
 
-        public void Add(int value)
+        public Node? Root() // Указатель на корневой элемент
+        {
+            if(Array[0] != null)
+            {
+                return Array[0];
+            }
+            return null;
+        }
+
+        public void Add(int value) // добавить новый узел
         {
             CurrentSize++;
-            Array[CurrentSize] = new Node(value, CurrentSize);
+            if(CurrentSize < _maxSize)
+            {
+                Array[CurrentSize] = new Node(value, CurrentSize); // Новый узел
+            }
+            else
+            {
+                Console.WriteLine("Max Size");
+                System.Environment.Exit(0); // Если масссив переполнен
+            }
         }
 
-        public void Print()
+        public void Print() 
         {
             for(int i = 0; i <= CurrentSize; i++)
             {
@@ -49,9 +72,30 @@ namespace _14lab
 
         public void AddValueToAll(int value)
         {
-            for (int i = 0; i <= CurrentSize; i++)
+            for (int i = 0; i <= CurrentSize; i++) // прибавление значение к каждому элементу
             {
                 Array[i].Add(value);
+            }
+        }
+
+        internal void CheckEqual()
+        {
+            int repeatedTimes = 0;
+            for(int i = 0; i <= CurrentSize; i++)
+            {
+                for(int l = 0; l <= CurrentSize; l++)
+                {
+                    if(Array[i].GetValue == Array[l].GetValue) 
+                    {
+                        repeatedTimes++; // Считаем сколько раз каждый элемент повторяется в массиве
+                    }
+                    
+                }
+                if(repeatedTimes == 1)
+                {
+                    Console.WriteLine(Array[i].GetValue + " non equal");
+                }
+                repeatedTimes = 0;
             }
         }
 
