058455365e5c7ca740453a31227fbf04b722361f
jejikeh <44530103+jejikeh@users.noreply.github.com>
2022-05-11 18:25:40 +0300

    22 lab pnyavu

---
+++ pnyavy/22lab/22lab/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _22lab
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            while(true)
+            {
+                Console.WriteLine("Input a number : ");
+                try
+                {
+                    Console.WriteLine(new Program().CalculateExpressions(Convert.ToDouble(Console.ReadLine())));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                 
+            }
+        }
+
+        public KeyValuePair<double, double> CalculateExpressions(double x)
+        {
+            if (x == 0 || x < 0)
+            {
+                throw new Exception("Wrong input number");
+            }
+            else
+            {
+                return new KeyValuePair<double, double>(
+                    Math.Sqrt(Math.Pow((3 * x + 2), 2) - (24 * x)) / (3 * Math.Sqrt(x) - (1 / 2 * Math.Sqrt(x))),
+                    -Math.Sqrt(x)
+                );
+            }
+        }
+    }
+
+    
+}
