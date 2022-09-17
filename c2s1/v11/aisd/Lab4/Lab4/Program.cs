2548708778d1709c990378ad89caedd1060e670c
jejikeh <jejikeh@gmail.com>
2022-09-17 05:45:41 +0300

    done

---
+++ c2s1/v11/aisd/Lab4/Lab4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        Dictionary<string, char> map = new Dictionary<string, char>();
+
+        string lengths = Console.ReadLine();
+        var l = lengths.Split(" ");
+        int uniqueChars = Int32.Parse(l.First());
+
+        for (int i = 0; i < uniqueChars; i++)
+        {
+            var input = Console.ReadLine().Split(": ");
+            map.Add(input.Last(), input.First()[0]);
+        }
+
+        string coded = Console.ReadLine();
+        string decoded = String.Empty;
+        
+        string token = string.Empty;
+        foreach (var ch in coded)
+        {
+            token += ch;
+            if (map.ContainsKey(token))
+            {
+                decoded += map[token];
+                token = String.Empty;
+            }
+        }
+        
+        Console.WriteLine(decoded);
+    }
+}
