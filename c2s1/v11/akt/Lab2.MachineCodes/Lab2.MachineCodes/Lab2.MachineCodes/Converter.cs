7be07c22401b85fbd80fa8e4a0a441b4d03fbf3c
Yauheni Lantsau <jejikeh@gmail.com>
2022-09-27 02:51:45 -0700

    added lost files

---
+++ c2s1/v11/akt/Lab2.MachineCodes/Lab2.MachineCodes/Lab2.MachineCodes/Converter.cs
@@ -0,0 +1,182 @@
+namespace Lab2.MachineCodes;
+
+public static class Converter
+{
+    internal static long DecimalToBinary(long num)
+    {
+        string res = String.Empty;
+        while (num > 0)
+        {
+            // add to the end of the string
+            res = (num % 2) + res;
+            num /= 2;
+        }
+        return long.Parse(res);
+    }
+
+    private static Dictionary<int, string> BsdTable = new()
+    {
+        {0,"0000"},
+        {1,"0001"},
+        {2,"0010"},
+        {3,"0011"},
+        {4,"0100"},
+        {5,"0101"},
+        {6,"0110"},
+        {7,"0111"},
+        {8,"1000"},
+        {9,"1001"}
+    };
+
+    internal static string DecimalToBCD(float num)
+    {
+        string res = String.Empty;
+        string numStr = num.ToString();
+        while (numStr.Length > 0)
+        {
+            if (numStr[0] == '.')
+            {
+                res += ". ";
+                numStr = numStr[1..];
+                continue;
+            }
+            res += BsdTable[int.Parse(numStr[0].ToString())] + " ";
+            numStr = numStr[1..];
+        }
+
+        return res;
+    }
+    
+    internal static double BSDtoDecimal(string num)
+    {
+        string res = String.Empty;
+        string[] numStr = num.ToString().Split(' ');
+
+        foreach (var code in numStr)
+        {
+            if (code == ".")
+            {
+                res += "."; continue;
+            }
+                
+            foreach (var decode in BsdTable.Where(x => x.Value == code))
+            {
+                res += decode.Key;
+            }
+        }
+
+        return double.Parse(res);
+    }
+
+    internal static string TextToAscii(string input)
+    {
+        string res = String.Empty;
+        foreach (int c in input.Where(x => x < 127))
+        {
+            res += c + " ";
+        }
+
+        return res;
+    }
+    
+    internal static string AsciiToText(string input)
+    {
+        string res = String.Empty;
+        foreach (string c in input.Split(' '))
+        {
+            if(c != String.Empty)
+                res += (char)int.Parse(c);
+        }
+        return res;
+    }
+
+    internal static string StraightCode(long number)
+    {
+        if (number < 0)
+        {
+            return  "1" + DecimalToBinary(number * -1).ToString();
+        }
+        
+        return  "0" + DecimalToBinary(number).ToString();
+    }
+
+    internal static string ReversedCode(long number)
+    {
+        if (number > 0)
+            return StraightCode(number);
+        
+        var res = StraightCode(number);
+        res = res[1..].Replace('0', '2')
+            .Replace('1', '0')
+            .Replace('2', '1');
+
+        return "1" + res;
+    }
+    
+    internal static string AdditionalCode(long number)
+    {
+        if (number > 0)
+            return Converter.StraightCode(number);
+        
+        return Calculator.Sum(long.Parse(Converter.ReversedCode(number)), 1).ToString();
+    }
+
+    internal static string Normalize(decimal number)
+    {
+        int l = 0;
+        if (Math.Abs(number) < 1)
+        {
+            int exp = 0;
+            while (Math.Abs(number) * 10 < 1)
+            {
+                number *= 10;
+                exp++;
+            }
+
+            while (number.ToString().Reverse().First() == '0')
+            {
+                number = decimal.Parse(number.ToString().Substring(0,number.ToString().Length - 1));
+            }
+            return number.ToString() + $" * 10^(-{exp})";
+        }
+        else
+        {
+            int exp = 0;
+            while (Math.Abs(number) > 1)
+            {
+                number /= 10;
+                exp++;
+            }
+            
+            while (number.ToString().Reverse().First() == '0')
+            {
+                number = decimal.Parse(number.ToString().Substring(0,number.ToString().Length - 1));
+            }
+            return number.ToString() + $" * 10^({exp})";
+        }
+    }
+
+    internal static double DeNormalize(string normalizedNumber)
+    {
+        normalizedNumber = normalizedNumber.Replace(" ", "");
+        
+        var powString = string.Empty;
+        while (normalizedNumber.Last() != '(')
+        {
+            if (normalizedNumber.Last() != ')')
+            {
+                powString = normalizedNumber.Last() + powString;
+            }
+            normalizedNumber = normalizedNumber[..^1];
+        }
+
+        var numberString = string.Empty;
+        while (normalizedNumber.First() != '*')
+        {
+            numberString += normalizedNumber.First();
+            normalizedNumber = normalizedNumber[1..];
+        }
+
+        return double.Parse(numberString) * Math.Pow(10, int.Parse(powString));
+    }
+}
---
+++ c2s1/v11/akt/Lab2.MachineCodes/Lab2.MachineCodes/Lab2.MachineCodes/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Lab2.MachineCodes;
+
+static class Program
+{
+    public static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+        
+        // число из десятичной системы счисления в двоично-десятичную
+        Console.WriteLine(Converter.DecimalToBCD(945.0f));
+        
+        // число из двоично-десятичной системы счисления в десятичную.
+        Converter.BSDtoDecimal(Converter.DecimalToBCD(444.125f));
+        
+        // Зашифруйте данный текст, используя таблицу ASCII-кодов
+        var coded = Converter.TextToAscii(
+            "Decoded Text. Lancev Evgeni Nikolaevich");
+        Console.WriteLine(coded);
+        
+        // Дешифруйте данный текст, используя таблицу ASCII-кодов
+        Console.WriteLine(Converter.AsciiToText(coded));
+        
+        // Представьте числа в прямом, обратном, дополнительном кодах
+        Console.WriteLine(Converter.StraightCode(-945));
+        Console.WriteLine(Converter.AdditionalCode(-945));
+        Console.WriteLine(Converter.ReversedCode(-945));
+        
+        // Выполните сложение чисел в обратом и дополнительном кодах
+        Console.WriteLine(Calculator.ReverseSum(399,309));
+        Console.WriteLine(Calculator.AdditionalSum(399,309));
+        
+        // Представьте числа в нормализованном виде
+        Console.WriteLine(Converter.Normalize(0.000945M));
+        Console.WriteLine(Converter.Normalize(945000M));
+        
+        // Выполните четыре арифметических действия над числами в формате с плавающей точкой
+        Console.WriteLine(Calculator.NormilizeSum(
+            Converter.Normalize(541000M),
+            Converter.Normalize(0.00322M)));
+        
+        Console.WriteLine(Calculator.NormilizeSubstract(
+            Converter.Normalize(7670000M),
+            Converter.Normalize(17700M)));
+        
+        Console.WriteLine(Calculator.NormilizeMutliply(
+            Converter.Normalize(58000M),
+            Converter.Normalize(0.0096M)));
+        
+        Console.WriteLine(Calculator.NormilizeDevide(
+            Converter.Normalize(74.1875M),
+            Converter.Normalize(15.3125M)));
+    }
+}
