8a2a78f16614b5ea7c3f5a05d07618c42e5574ce
jejikeh <jejikeh@gmail.com>
2023-02-11 02:10:05 +0300

    rpi 1lab

--- c2s2/rpi/lab1/lab.bat
+++ c2s2/rpi/lab1/lab.bat
@@ -1,50 +1,58 @@
 @echo off
 :start
-echo 1 - upleft
-echo 2 - upright
-echo 3 - downleft
-echo 4 - downright
+echo 1 - down
+echo 2 - right
+echo 3 - left
 set /p var=Option:
-if "%var%" == "1" goto upleft
-if "%var%" == "2" goto upright
-if "%var%" == "3" goto downleft
-if "%var%" == "4" goto downright
-
-
-:upleft
-echo 1 - exit
-echo 2 - next    
-set /p var=Option:
-if "%var%" == "1" goto exit
-if "%var%" == "2" goto start
-
-
-:upright
-echo 1 - exit
-echo 2 - next    
-set /p var=Option:
-if "%var%" == "1" goto exit
-if "%var%" == "2" goto start
-
-
-:downleft
-echo 1 - exit
-echo 2 - next    
-set /p var=Option:
-if "%var%" == "1" goto exit
-if "%var%" == "2" goto down
-
-
-:downright
-echo 1 - exit
-echo 2 - next    
-set /p var=Option:
-if "%var%" == "1" goto exit
-if "%var%" == "2" goto down
+if "%var%" == "1" goto down
+if "%var%" == "2" goto right
+if "%var%" == "3" goto left
 
 
 :down
+echo 1 - exit
+echo 2 - next    
+set /p var=Option:
+if "%var%" == "1" goto exit
+if "%var%" == "2" goto down1
+
+:down1
+echo 1 - exit
+echo 2 - next    
+set /p var=Option:
+if "%var%" == "1" goto exit
+if "%var%" == "2" goto down2
+
+:down2
+echo 1 - back
+echo 2 - start  
+set /p var=Option:
+if "%var%" == "1" goto down
+if "%var%" == "2" goto start
+
+:left
+echo 1 - next   
+echo 2 - exit  
+set /p var=Option:
+if "%var%" == "1" goto left1
+if "%var%" == "2" goto exit
+
+:left1
+echo 1 - next   
+set /p var=Option:
+if "%var%" == "1" goto down
+
+
+:right
+echo 1 - exit
+echo 2 - next    
+set /p var=Option:
+if "%var%" == "1" goto exit
+if "%var%" == "2" goto right1
+
+:right1
 echo 1 - start
 set /p var=Option:
 if "%var%" == "1" goto start
+
 :exit
