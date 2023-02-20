336f2bf9ffd7bf5118278d879f5cfd787698b901
jejikeh <jejikeh@gmail.com>
2023-02-20 18:26:01 +0300

    update otp lab2

---
+++ c2s2/ootp/Lab2/Lab2/Lab2/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace Lab1;
+
+public static class Program
+{
+    private static EventManager _eventManager = new EventManager();
+    public static void Main()
+    {
+        Console.WriteLine("1. Add\n" +
+                          "2. Remove\n" +
+                          "3. Edit\n" +
+                          "4. Look all\n" +
+                          "5. Find event\n" +
+                          "6. Add Range");
+
+        var task = ParseNumber();
+
+        switch (task)
+        {
+            case 1:
+                _eventManager.Add(SomeEvent.CreateFromKeyboard());
+                break;
+            case 2:
+                if (_eventManager.Count == 0)
+                {
+                    Console.WriteLine("EventManager is empty!");
+                    break;
+                }
+
+                Console.WriteLine("Input index: ");
+                var index = ParseNumber();
+                while (index > _eventManager.Count)
+                    index = ParseNumber();
+
+                _eventManager.Remove(index);
+                
+                break;
+            case 3:
+                Console.WriteLine("Input title");
+                var inputTitle = Console.ReadLine();
+                while (inputTitle is null)
+                    inputTitle = Console.ReadLine();
+
+                var item = _eventManager.Find(inputTitle);
+                if (item is null)
+                    Console.WriteLine("No such item in manager");
+                else
+                    item.EditFromKeyboard();
+                break;
+            case 4:
+                for (var i = 0; i < _eventManager.Count; i++)
+                    Console.WriteLine($"{i}: {_eventManager[i]}");
+                break;
+            case 5:
+                Console.Write("Input Date: ");
+                DateTime date;
+                var inputDate = Console.ReadLine();
+                while (!DateTime.TryParse(inputDate, out date))
+                    inputDate = Console.ReadLine();
+
+                Console.WriteLine("Event at this day:\n");
+                foreach (var someEvent in _eventManager.Find(date))
+                    Console.WriteLine(someEvent + "\n------");
+                break;
+            case 6:
+                if (_eventManager.Count == 0)
+                {
+                    Console.WriteLine("EventManager is empty!");
+                    break;
+                }
+                Console.WriteLine("Input copied index");
+                var tempIndex = 0;
+                var inputIndex = Console.ReadLine();
+                while (!int.TryParse(inputIndex, out tempIndex))
+                    inputIndex = string.Empty;
+
+                if (_eventManager.Count < tempIndex)
+                {
+                    Console.WriteLine($"Index too big, in event manager only {_eventManager.Count} items");
+                    break;
+                }
+                
+                inputIndex = string.Empty;
+                Console.WriteLine("Input count");
+                var tempCount = 0;
+                while (!int.TryParse(inputIndex, out tempCount))
+                    inputIndex = Console.ReadLine();
+                
+                _eventManager.AddRange(_eventManager[tempIndex], tempCount);
+                break;
+        }
+
+        Main();
+    }
+    
+    private static int ParseNumber()
+    {
+        var input = Console.ReadLine();
+        var task = 0;
+        while(!int.TryParse(input, out task) && (task is > 0 and < 5))
+            input = Console.ReadLine();
+
+        return task;
+    }
+}
---
+++ c2s2/ootp/Lab2/Lab2/Lab2/SomeEvent.cs
@@ -0,0 +1,81 @@
+﻿using System.Globalization;
+
+namespace Lab1;
+
+public class SomeEvent
+{
+    public string Title { get; private set; }
+    public DateTime Date { get; private set; }
+
+    public SomeEvent()
+    {
+        Title = "unset";
+        Date = DateTime.Today;
+        Console.WriteLine($"{Title} empty constructor was was called");
+    }
+
+    public SomeEvent(SomeEvent someEvent)
+    {
+        Title = someEvent.Title;
+        Date = someEvent.Date;
+        Console.WriteLine($"{Title} copy constructor was was called");
+    }
+
+    public SomeEvent(string title, DateTime date)
+    {
+        Title = title;
+        Date = date;
+        Console.WriteLine($"{Title} constructor with parameters was was called");
+    }
+
+    ~SomeEvent()
+    {
+        Console.WriteLine($"{Title} destructor was called");
+        Title = "";
+        Date = DateTime.Now;
+    }
+
+    public override string ToString()
+    {
+        return $"Title: {Title} Date: {Date}";
+    }
+
+    public static SomeEvent CreateFromKeyboard()
+    {
+        Console.Write("Input Event title: ");
+        var title = Console.ReadLine();
+        while (title == string.Empty)
+            title = Console.ReadLine();
+        
+        Console.Write("Input Date title in style mm/dd/yy hh:mm:ss: ");
+        DateTime date;
+        var input = Console.ReadLine();
+        while (!DateTime.TryParseExact(input, "G",CultureInfo.InvariantCulture,DateTimeStyles.None, out date))
+            input = Console.ReadLine();
+
+
+        return new SomeEvent()
+        {
+            Title = title,
+            Date = date
+        };
+    }
+    
+    public SomeEvent EditFromKeyboard()
+    {
+        Console.Write("Input Event title: ");
+        var title = Console.ReadLine();
+        while (title is null)
+            title = Console.ReadLine();
+        
+        Console.Write("Input Date title in style mm/dd/yy hh:mm:ss: ");
+        DateTime date;
+        var input = Console.ReadLine();
+        while (!DateTime.TryParseExact(input, "G",CultureInfo.InvariantCulture, DateTimeStyles.None,out date))
+            input = Console.ReadLine();
+
+        Date = date;
+        Title = title;
+        return this;
+    }
+}
