1bc81d849e901cbc10b5d77585e62537221f0c9e
jejikeh <jejikeh@gmail.com>
2023-03-12 00:15:42 +0300

    remove unused psu site on next.js

--- c1s2/practice/psu/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Button = ({children}) => {
-    return (
-        <div className="bgbut">
-        <button className="but">{children}</button>
-        </div>
-    );
-}
