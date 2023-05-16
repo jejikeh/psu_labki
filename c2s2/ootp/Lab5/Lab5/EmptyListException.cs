eba4060241fdd1eb45bd64a61ae5ff8c0de429fb
jejikeh <jejikeh@gmail.com>
2023-05-16 21:56:49 +0300

    WIP course

---
+++ c2s2/ootp/Lab5/Lab5/EmptyListException.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab5;
+
+public class CustomException : Exception
+{
+    public CustomException(string message)
+        : base(message)
+    {
+    }
+
+    public CustomException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
---
+++ c2s2/ootp/Lab5/Lab5/GenericEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace Lab5;
+
+public class GenericEvent
+{
+    
+}
